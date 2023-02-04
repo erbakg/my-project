6ecd00ef1781fba2a31812e21f2b3fd64b7ba1be
user <user@users-MacBook-Pro-3.local>
2023-02-04 14:46:06 +0600

    Added react react router, set lazy loading of components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import { Counter } from './components/Counter';
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './index.scss';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
 
 export default function App() {
   return (
     <div className="app">
-      dsdsdsds
-      <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
