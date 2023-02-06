d74e0249cf24a9ed6ee0f63ca38fc9a744c03c97
user <user@users-MacBook-Pro-3.local>
2023-02-06 11:39:06 +0600

    Added routeConfig with path and elements

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,6 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { Suspense } from 'react';
 import { Link } from 'react-router-dom';
-import { Route, Routes } from 'react-router-dom';
 import classNames from 'shared/lib/classNames/classNames';
+import { AppRouter } from './providers/router';
 import { useTheme } from './providers/ThemeProvider';
 import './styles/index.scss';
 
@@ -15,12 +12,7 @@ export default function App() {
       <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={'/about'} element={<AboutPage />} />
-          <Route path={'/'} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 }
