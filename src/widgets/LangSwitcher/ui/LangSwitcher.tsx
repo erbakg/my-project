c9bd0a670c0109d9ed011579087432b00c225f00
user <user@users-MacBook-Pro-3.local>
2023-02-11 16:52:06 +0600

    added stylelint and i18next plugin, fix files

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import classNames from 'shared/lib/classNames/classNames';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import cls from './LangSwitcher.module.scss';
 
 interface LangSwitcherProps {
   className?: string;
@@ -14,7 +13,7 @@ export const LangSwitcher = ({ className }: LangSwitcherProps) => {
         i18n.changeLanguage(i18n.language === 'ru' ? 'en' : 'ru');
     };
     return (
-        <div className={classNames(cls.LangSwitcher, {}, [className])}>
+        <div className={classNames('', {}, [className])}>
             <Button theme={ThemeButton.CLEAR} onClick={toggle}>
                 {t('Language')}
             </Button>
